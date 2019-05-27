7a2b4f23f2141fbd2706ad311e3c3766f351b6bd
pedro <pedrobrunofpinto@gmail.com>
2019-05-27 10:18:10 +0100

    totop fix

--- assets/javascript/func.js
+++ assets/javascript/func.js
@@ -13,15 +13,19 @@ $(document).ready(function() {
     }, x);
 });
 
-$( document ).ready(function() {
-  $(document).scroll(function() {
-    var y = $(this).scrollTop();
-    if (y > 100) {
-      $('#toTop').fadeIn();
+
+// ===== Scroll to Top ==== 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
     } else {
-      $('#toTop').fadeOut();
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
     }
-  });
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
 });
 
 $(window).on( 'scroll', function() {
@@ -43,9 +47,7 @@ $(document).ready(function() {
         $input.change();
         return false;
       });
-      $("#toTop").click(function() {
-        $("html, body").animate({scrollTop: 0}, 1000);
-  });
+
 
     });
 
