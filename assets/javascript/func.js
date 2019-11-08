d475f9662adfbf74b2453181e161590e961e0ec8
pedro <pedrobrunofpinto@gmail.com>
2019-11-08 14:31:39 +0000

    Update func.js

--- assets/javascript/func.js
+++ assets/javascript/func.js
@@ -1155,18 +1155,21 @@ $( document ).ready(function() {
 
 
 
+  ngOnInit() {
+    // Detects if device is on iOS 
+    const isIos = () => {
+      const userAgent = window.navigator.userAgent.toLowerCase();
+      return /iphone|ipad|ipod/.test( userAgent );
+    }
+    // Detects if device is in standalone mode
+    const isInStandaloneMode = () => ('standalone' in (window as any).navigator) && ((window as any).navigator.standalone);
 
-
-// Detects if device is on iOS 
-const isIos = () => {
-  const userAgent = window.navigator.userAgent.toLowerCase();
-  return /iphone|ipad|ipod/.test( userAgent );
-}
-// Detects if device is in standalone mode
-const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
-
-// Checks if should display install popup notification:
-if (isIos() && !isInStandaloneMode()) {
-  this.setState({ showInstallMessage: true });
-}
-
+    // Checks if should display install popup notification:
+    if (isIos() && !isInStandaloneMode()) {
+      this.toast.openFromComponent(IosInstallComponent, { 
+        duration: 8000,
+        horizontalPosition: 'start', 
+        panelClass: ['mat-elevation-z3'] 
+      });
+    }
+  }
