4602a378d027c0d8926bac2b07fb13599edb56d9
pedro <pedrobrunofpinto@gmail.com>
2019-11-08 14:52:26 +0000

    jh

--- assets/javascript/func.js
+++ assets/javascript/func.js
@@ -1151,25 +1151,17 @@ $( document ).ready(function() {
 });
 
 
+// Detects if device is on iOS 
+const isIos = () => {
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  return /iphone|ipad|ipod/.test( userAgent );
+}
+// Detects if device is in standalone mode
+const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+// Checks if should display install popup notification:
+if (isIos() && !isInStandaloneMode()) {
+  this.setState({ showInstallMessage: true });
+}
 
 
-
-
-  ngOnInit() {
-    // Detects if device is on iOS 
-    const isIos = () => {
-      const userAgent = window.navigator.userAgent.toLowerCase();
-      return /iphone|ipad|ipod/.test( userAgent );
-    }
-    // Detects if device is in standalone mode
-    const isInStandaloneMode = () => ('standalone' in (window as any).navigator) && ((window as any).navigator.standalone);
-
-    // Checks if should display install popup notification:
-    if (isIos() && !isInStandaloneMode()) {
-      this.toast.openFromComponent(IosInstallComponent, { 
-        duration: 8000,
-        horizontalPosition: 'start', 
-        panelClass: ['mat-elevation-z3'] 
-      });
-    }
-  }
